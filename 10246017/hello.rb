3e50da9ddb069a6f4b38f32a9b1ae344a144ca69
Salonpas <fish840212@yahoo.com.tw>
2016-03-17 16:47:55 +0800

    0317-1

--- 10246017/hello.rb
+++ 10246017/hello.rb
@@ -1,1 +1,14 @@
-puts ("hello")
+puts ("李哲宇")
+print ("print 印出字串內的字\nputs 印出字串內的字變換行\np印出完整字串內容包含框架\n")
+puts ("|f|是一個變數")
+
+
+  friends = ['aa', 'bb', 'cc']
+  friends.each do |f|
+    puts f
+  end
+
+1.upto(10) {|x|
+	puts x }
+
+p (1..100).to_a.shuffle.first(5)
